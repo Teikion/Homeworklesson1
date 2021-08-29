7e0296d4987dd68951a0428be00137c2818c53fa
Teikion <Lupin721@mail.ru>
2021-08-29 19:37:59 +0300

    Initial commit

---
+++ src/main/java/Homework/HomeWork.java
@@ -0,0 +1,62 @@
+package Homework;
+
+import javax.naming.Name;
+
+public class HomeWork {
+    public static void main(String[] args) {
+        System.out.println("Hello Study");
+        byte byteOne = 8;
+        System.out.println(byteOne);
+        short shortOne = 2502;
+        System.out.println(shortOne);
+        int integerOne = 23243;
+        System.out.println(integerOne);
+        long longOne = 21321030L;
+        System.out.println(longOne);
+        char charOne ='f';
+        char charOne1 = 35;
+        System.out.println(charOne1);
+        boolean booleanOne = true;
+        boolean booleanOne1= (12-10) < 5;
+        System.out.println(booleanOne1);
+        double doubleOne = 0.53;
+        System.out.println(doubleOne);
+        float floatOne = 42.53f;
+
+        String StringOne = "Мир";
+        String StringOne1 = " всем";
+        System.out.println(StringOne+StringOne1);}
+    //3
+    public static float thrid(float a, float b, float c, float d){
+        a =7f;
+        b =30f;
+        c=2f;
+        d=-6f;
+        return a * (b + (c / d));
+
+
+
+        //4
+
+         static boolean check(int one, int two);{
+
+        return one+two <= 20 && one + two >= 10;
+
+        }
+         static void fifth(int m){
+            System.out.println(m < 0 ? "-" : "+");
+
+
+            public static boolean sixth(int i){
+                return i < 0;
+
+                public void methodName(String Name) {
+                    System.out.println("Привет, " + Name + "!");
+
+
+
+
+}
+
+
+}
